f7d89de9e71d6246b3af8cf5a63a6c5268866522
FuluZwivhuya <148289949+FuluZwivhuya@users.noreply.github.com>
2024-02-25 21:25:52 +0200

    changes made to buttons according to status map

--- scripts.js
+++ scripts.js
@@ -34,32 +34,32 @@ statuz[1].style.color = STATUS_MAP.reserved.color;
 statuz[2].style.color = STATUS_MAP.shelf.color;*/
 
 checkinz = document.getElementsByClassName('checkin')
-console.log(checkinz)
+console.log(checkinz);
 /*checkinz[0].style.color = 'black';
 checkinz[1].style.color = 'black';
 checkinz[2].style.color = 'black';*/
 
 reservez = document.getElementsByClassName('reserve')
-console.log(reservez)
+console.log(reservez);
 checkoutz = document.getElementsByClassName('checkout')
-console.log(checkoutz)
+console.log(checkoutz);
 
 checkinz[0].style.color = 'black';
 statuz[0].style.color = STATUS_MAP.overdue.color;
-//reservez[0]= STATUS_MAP.status.canReserver ? 'enable':'disabled';
-//checkoutz[0] = STATUS_MAP.status.canCheckout ? 'disabled' : 'disabled';
-//checkinz[0]= STATUS_MAP.status.canCheckIn ? 'enabled' : 'disabled';
+reservez[0]= STATUS_MAP.overdue.canReserve;
+checkoutz[0] = STATUS_MAP.overdue.canCheckout;
+checkinz[0]= STATUS_MAP.overdue.canCheckIn;
 
 checkinz[1].style.color = 'black';
 statuz[1].style.color = STATUS_MAP.reserved.color;
-//reservez[1] = STATUS_MAP.status.canReserver ? 'disbaled' : 'disabled';
-//checkoutz[1] = STATUS_MAP.status.canCheckout ? 'enabled' : 'disabled';;
-//checkinz[1] = STATUS_MAP.status.canCheckIn ? 'disabled' : 'disabled'
+reservez[1] = STATUS_MAP.reserved.canReserve;
+checkoutz[1] = STATUS_MAP.reserved.canCheckout;
+checkinz[1] = STATUS_MAP.reserved.canCheckIn;
 
 checkinz[2].style.color = 'black';
 statuz[2].style.color = STATUS_MAP.shelf.color;
-//reservez[2] = STATUS_MAP.status.canReserver ? 'enabled' : 'disabled';
-//checkout[2] = STATUS_MAP.status.canCheckout ? 'enabled' : 'disabled';
-//checkinz[2] = STATUS_MAP.status.canCheckIn ? 'disbaled' : 'disabled';
+reservez[2] = STATUS_MAP.shelf.canReserve;
+checkout[2] = STATUS_MAP.shelf.canReserve;
+checkinz[2] = STATUS_MAP.shelf.canCheckIn;
 
 
